d7f11255ed9677399ba2044439585b67c6e02b6b
LUMI <yin9423@gmail.com>
2026-06-26 11:10:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-setTimeout(()=>{
-document.getElementById('loading').style.display='none';
-document.getElementById('app').classList.remove('hidden');
-},1800);
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("boot").style.display = "none";
+    document.getElementById("site").classList.remove("hidden");
+  }, 1900);
+});
+
+document.querySelectorAll(".module").forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    document.body.style.setProperty("--cyan", "#8df3ff");
+  });
+  item.addEventListener("mouseleave", () => {
+    document.body.style.setProperty("--cyan", "#69e8ff");
+  });
+});
