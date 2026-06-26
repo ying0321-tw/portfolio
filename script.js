9de82384c36efa337dded033397449197b963c87
LUMI <yin9423@gmail.com>
2026-06-26 11:52:20 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href="#"]').forEach(link => {
-  link.addEventListener("click", event => {
-    event.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-});
